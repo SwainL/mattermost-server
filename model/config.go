1c8b09d4e18e04f60e1fb7eb2c1b37e4977566ee
Martin Kraft <martinkraft@gmail.com>
2019-01-28 11:44:28 -0500

    Changes config setting name.

--- model/config.go
+++ model/config.go
@@ -1015,7 +1015,7 @@ type EmailSettings struct {
 	RequireEmailVerification          bool
 	FeedbackName                      string
 	FeedbackEmail                     string
-	FeedbackReplyEmail                string
+	ReplyToAddress                    string
 	FeedbackOrganization              *string
 	EnableSMTPAuth                    *bool
 	SMTPUsername                      *string
